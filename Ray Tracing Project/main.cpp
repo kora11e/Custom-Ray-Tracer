1e3fa023c7a3e363996296029632b7bd93fbfe94
kora11e <87006301+kora11e@users.noreply.github.com>
2024-03-24 23:27:22 +0100

    Background Ray Hit Implementation

--- Ray Tracing Project/main.cpp
+++ Ray Tracing Project/main.cpp
@@ -1,21 +1,17 @@
+#include "rtweekend.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "Sphere.h"
 #include <iostream>
 #include <cstdio>
 #include "color.h"
 #include "Ray.h"
 #include "Vector3.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
-}
-
-color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,1), 0.5, r)) {
-        return color(1, 0, 0);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -51,6 +47,12 @@ int main()
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
+    //World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, 100.5, -1), 100));
+
     //Render
     printf("%d", image_width);
     printf("%d", image_height);
@@ -65,7 +67,7 @@ int main()
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
